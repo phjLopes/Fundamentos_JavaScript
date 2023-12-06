487a193de9e0f5b87f9d3c95bb360744fe0729d2
phjLopes <153042917+phjLopes@users.noreply.github.com>
2023-12-05 23:46:28 -0300

    Fundamentos JS

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dados
+// booleanos
+
+// conversao implicita
+ const numero = 456;
+ const numeroString ="456";
+
+ // number() para transformar uma string em um numero
+ // String() para converter um  numero em string
+
+ console.log(numero + Number(numeroString));
+
+ 
+// conversao explicita
+
---
+++ truthy-false.js
@@ -0,0 +1,30 @@
+//uso de boolean
+
+const usuarioLogado = true
+const contapaga = false
+
+// truthy oy false
+
+//0 =>false
+//1 => true
+
+console.log(0 ==false)
+console.log('' ==false)
+console.log(1 ==false)
+
+//null => vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof varNull)
+console.log(typeof minhaVar)
+
+//null e =>objeto
+
+
+
