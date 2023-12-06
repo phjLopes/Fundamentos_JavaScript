a40162dca652d6bfb323d2cf55f330498d54526b
phjLopes <153042917+phjLopes@users.noreply.github.com>
2023-12-05 23:23:54 -0300

    Uso de variáveis var, let e const

---
+++ script.js
@@ -0,0 +1,39 @@
+// usando o  var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area)
+//var area;
+
+
+// Exemplo de let
+//let forma ='retangulo'
+//let altura =5;
+//let comprimento =7;
+//let area;
+
+ //if(forma ==='retangulo'){
+ //area = altura * comprimento;
+ //}else{
+ //    area = altura * comprimento/2;
+
+
+// console.log(area)
+
+
+//usando const
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area ;
+
+if(forma ==="quadrado"){
+    area = altura * comprimento
+} else{
+    area = (altura * comprimento)/2;
+}
+
+console.log(area)
